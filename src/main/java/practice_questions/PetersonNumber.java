39826f38d67ff188908f6e8a794fce050a753299
akshaybho <bhogale.akshay11@gmail.com>
2025-05-28 20:48:24 +0530

    new update_4

--- src/main/java/practice_questions/PetersonNumber.java
+++ src/main/java/practice_questions/PetersonNumber.java
@@ -4,35 +4,49 @@ import java.util.Scanner;
 
 public class PetersonNumber {
 
-    public static void main(String[] args) {
+    Scanner sc;
+    int n;
 
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args)
+    {
+        PetersonNumber t = new PetersonNumber();
+        t.start_application();
+    }
+    void start_application()
+    {
         System.out.println("Enter a number : ");
-        int n = sc.nextInt();
-        String v = String.valueOf(n);
-        int []a = new int[v.length()];
+        getUserInput();
+        main_logic();
+    }
+    void getUserInput()
+    {
+        sc = new Scanner(System.in);
+        n = sc.nextInt();
+    }
+    void main_logic()
+    {
         int sum = 0;
         int h = n;
+        String s = String.valueOf(n);
+        int []a = new int[s.length()];
+
         for(int i=0; i<a.length; i++)
         {
             int b = n%10;
             n = n/10;
-
             a[i] = b;
-
         }
+
         for(int j=0; j<a.length; j++)
         {
             int fact = 1;
-            for(int k=a[j]; k>=1; k--)
+            for(int k = a[j]; k>=1; k--)
             {
-                fact = fact * k;
-
+                fact = fact*k;
             }
             sum = sum + fact;
-
         }
-        if(h==sum)
+        if(sum==h)
         {
             System.out.println("Peterson Number");
         }
@@ -40,7 +54,6 @@ public class PetersonNumber {
         {
             System.out.println("Not Peterson Number");
         }
-
-
+        start_application();
     }
 }
